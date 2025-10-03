a871d35504d1202aecfe0ac6d8bc495ea4a0ddc3
Leoauchana <matiasleonelcja@gmail.com>
2025-10-03 20:37:30 -0300

    Agregado

---
+++ Backend-AR.TFI/Tfi.Api/Controllers/ProyectController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tfi.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProyectController : ControllerBase
+{
+    public ProyectController()
+    {
+
+    }
+
+}
